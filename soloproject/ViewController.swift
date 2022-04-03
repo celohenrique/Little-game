1d60328aed3b1d5cc77878b5046060027af81653
Marcelo Henrique <celohenriquech7@gmail.com>
2022-04-03 20:24:39 -0300

    Initial Commit

---
+++ soloproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  soloproject
+//
+//  Created by Marcelo Araujo on 03/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
